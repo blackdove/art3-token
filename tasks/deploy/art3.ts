1a2554793a90605e0cb593645d4a15b52e4147c0
Derek 💫 <arein@paypal.com>
2021-11-30 10:57:49 -0500

    misc

--- tasks/deploy/art3.ts
+++ tasks/deploy/art3.ts
@@ -1,12 +1,21 @@
 import { task } from "hardhat/config";
-import { TaskArguments } from "hardhat/types";
+import { Signer } from "@ethersproject/abstract-signer";
 
 import { ART3 } from "../../src/types/ART3";
 import { ART3__factory } from "../../src/types/factories/ART3__factory";
 
 task("deploy:ART3")
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const art3Factory: ART3__factory = <ART3__factory>await ethers.getContractFactory("ART3");
+  .addOptionalParam('safe', 'The address of a safe')
+  .setAction(async function ({ safe }, { ethers }) {
+    const accounts: Signer[] = await ethers.getSigners();
+    let signer;
+    if (safe !== undefined) {
+      const service = new SafeService("https://safe-transaction.gnosis.io/")
+      signer = await SafeEthersSigner.create("some_safe_address", accounts[], service)
+    } else {
+      signer = accounts[0];
+    }
+    const art3Factory: ART3__factory = <ART3__factory>await ethers.getContractFactory("ART3", signer);
     const art3: ART3 = <ART3>await art3Factory.deploy();
     await art3.deployed();
     console.log("ART3 deployed to: ", art3.address);
