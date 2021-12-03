4371694f885894f25f7db89e1985f57840a048f3
Derek 💫 <arein@paypal.com>
2021-12-03 10:07:57 -0500

    misc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -68,6 +68,7 @@ const config: HardhatUserConfig = {
     kovan: getChainConfig("kovan"),
     rinkeby: getChainConfig("rinkeby"),
     ropsten: getChainConfig("ropsten"),
+    mainnet: getChainConfig("mainnet"),
   },
   paths: {
     artifacts: "./artifacts",
--- tasks/deploy/art3.ts
+++ tasks/deploy/art3.ts
@@ -7,21 +7,32 @@ import { ART3 } from "../../src/types/ART3";
 import { ART3__factory } from "../../src/types/factories/ART3__factory";
 import { SafeService, SafeEthersSigner } from "@gnosis.pm/safe-ethers-adapters"
 
+const safe_service: Map<String, String> = new Map<string, string>();
+safe_service.set('rinkeby', 'https://safe-transaction.rinkeby.gnosis.io/');
+safe_service.set('mainnet', 'https://safe-transaction.gnosis.io/');
+
+const etherscan_uri: Map<String, String> = new Map<string, string>();
+etherscan_uri.set('rinkeby', 'https://rinkeby.etherscan.io/');
+etherscan_uri.set('mainnet', 'https://etherscan.io/');
+
 task("deploy:ART3")
   .addOptionalParam('safe', 'The address of a safe')
-  .setAction(async function ({ safe }, { ethers }) {
+  .addOptionalParam('targetnetwork', 'The network to deploy to', 'mainnet')
+  .setAction(async function ({ safe, targetnetwork }, { ethers }) {
     const accounts: Signer[] = await ethers.getSigners();
     let signer: Signer = accounts[0];
     if (safe !== undefined) {
-      const provider = new JsonRpcProvider('https://rinkeby.infura.io/v3/1c96603e022b4d97a08b70a95afae845');
-      const service = new SafeService("https://safe-transaction.rinkeby.gnosis.io/");
+      const provider: JsonRpcProvider = new JsonRpcProvider(`https://${targetnetwork}.infura.io/v3/${process.env.INFURA_API_KEY}`);
+      const serviceUrl = `${safe_service.get(targetnetwork)!}`;
+      const service = new SafeService(serviceUrl);
       const ethAdapter = new EthersAdapter({ ethers, signer })
-      const s = await Safe.create({ ethAdapter, safeAddress: '0xFE68454B0d5845687685e3BD9AaCece8988617BC' })
+      const s = await Safe.create({ ethAdapter, safeAddress: safe })
       signer = new SafeEthersSigner(s, service, provider);
     }
     const art3Factory: ART3__factory = <ART3__factory>await ethers.getContractFactory("ART3", signer);
     const art3: ART3 = <ART3>await art3Factory.deploy();
     await art3.deployed();
     console.log("ART3 deployed to: ", art3.address);
-    console.log(`View it on etherscan: https://rinkeby.etherscan.io/token/${art3.address}`);
+    const etherscanUrl = `${etherscan_uri.get(targetnetwork)!}`;
+    console.log(`View it on etherscan: ${etherscanUrl}token/${art3.address}`);
   });
