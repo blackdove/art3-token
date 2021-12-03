e7794d185f42bf0e573cc0712a0aef9605d8d692
Derek <alexanderderekrein@gmail.com>
2021-12-03 10:09:16 -0500

    Adds feature to make token non-transferrable (#2)

--- tasks/deploy/art3.ts
+++ tasks/deploy/art3.ts
@@ -1,14 +1,38 @@
 import { task } from "hardhat/config";
-import { TaskArguments } from "hardhat/types";
+import { Signer } from "@ethersproject/abstract-signer";
+import Safe, { EthersAdapter } from '@gnosis.pm/safe-core-sdk';
+import { JsonRpcProvider } from '@ethersproject/providers'
 
 import { ART3 } from "../../src/types/ART3";
 import { ART3__factory } from "../../src/types/factories/ART3__factory";
+import { SafeService, SafeEthersSigner } from "@gnosis.pm/safe-ethers-adapters"
+
+const safe_service: Map<String, String> = new Map<string, string>();
+safe_service.set('rinkeby', 'https://safe-transaction.rinkeby.gnosis.io/');
+safe_service.set('mainnet', 'https://safe-transaction.gnosis.io/');
+
+const etherscan_uri: Map<String, String> = new Map<string, string>();
+etherscan_uri.set('rinkeby', 'https://rinkeby.etherscan.io/');
+etherscan_uri.set('mainnet', 'https://etherscan.io/');
 
 task("deploy:ART3")
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const art3Factory: ART3__factory = <ART3__factory>await ethers.getContractFactory("ART3");
+  .addOptionalParam('safe', 'The address of a safe')
+  .addOptionalParam('targetnetwork', 'The network to deploy to', 'mainnet')
+  .setAction(async function ({ safe, targetnetwork }, { ethers }) {
+    const accounts: Signer[] = await ethers.getSigners();
+    let signer: Signer = accounts[0];
+    if (safe !== undefined) {
+      const provider: JsonRpcProvider = new JsonRpcProvider(`https://${targetnetwork}.infura.io/v3/${process.env.INFURA_API_KEY}`);
+      const serviceUrl = `${safe_service.get(targetnetwork)!}`;
+      const service = new SafeService(serviceUrl);
+      const ethAdapter = new EthersAdapter({ ethers, signer })
+      const s = await Safe.create({ ethAdapter, safeAddress: safe })
+      signer = new SafeEthersSigner(s, service, provider);
+    }
+    const art3Factory: ART3__factory = <ART3__factory>await ethers.getContractFactory("ART3", signer);
     const art3: ART3 = <ART3>await art3Factory.deploy();
     await art3.deployed();
     console.log("ART3 deployed to: ", art3.address);
-    console.log(`View it on etherscan: https://rinkeby.etherscan.io/token/${art3.address}`);
+    const etherscanUrl = `${etherscan_uri.get(targetnetwork)!}`;
+    console.log(`View it on etherscan: ${etherscanUrl}token/${art3.address}`);
   });
--- test/types.ts
+++ test/types.ts
@@ -1,8 +1,11 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import type { Fixture } from "ethereum-waffle";
 
+import type { ART3 } from "../src/types/ART3";
+
 declare module "mocha" {
   export interface Context {
+    art3: ART3;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
